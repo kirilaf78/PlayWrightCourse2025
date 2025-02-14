79478ef8a35ac2d2e566a020ffe70cf4445177cc
Kiryl <kirilaf78@gmail.com>
2025-02-14 02:45:02 +0100

    initial commit

---
+++ tests/recordTest1.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("https://github.com/");
+  await page.getByRole("link", { name: "Sign in" }).click();
+  await page.getByRole("textbox", { name: "Username or email address" });
+  await page.getByRole("textbox", { name: "Username or email address" }).fill("some");
+  await page.getByRole("textbox", { name: "Password" }).click();
+  await page.getByRole("textbox", { name: "Password" }).fill("1234");
+  await page.getByRole("button", { name: "Sign in", exact: true }).click();
+  await expect(page.getByRole("alert")).toContainText(
+    "Incorrect username or password."
+  );
+});
