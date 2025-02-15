1b544a21b3529202624da09c371eecd72bc163ee
Kiryl <kirilaf78@gmail.com>
2025-02-15 02:06:47 +0100

    some changes to config and initial test

--- tests/recordTest1.spec.ts
+++ tests/recordTest1.spec.ts
@@ -1,14 +1,24 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("test", async ({ page }) => {
-  await page.goto("https://github.com/");
-  await page.getByRole("link", { name: "Sign in" }).click();
-  await page.getByRole("textbox", { name: "Username or email address" });
-  await page.getByRole("textbox", { name: "Username or email address" }).fill("some");
-  await page.getByRole("textbox", { name: "Password" }).click();
-  await page.getByRole("textbox", { name: "Password" }).fill("1234");
-  await page.getByRole("button", { name: "Sign in", exact: true }).click();
-  await expect(page.getByRole("alert")).toContainText(
-    "Incorrect username or password."
-  );
+test('GitHub negative test', async ({ page }) => {
+  await test.step('Navigate to URL', async () => {
+    await page.goto('https://github.com/');
+    await page.getByRole('link', { name: 'Sign in' }).click();
+  });
+  await test.step('Enter userName and password', async () => {
+    await page
+      .getByRole('textbox', { name: 'Username or email address' })
+      .fill('some');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('1234');
+  });
+
+  await test.step('Click on sign in', async () => {
+    await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+  });
+  await test.step('Validate error message', async () => {
+    await expect(page.getByRole('alert')).toContainText(
+      'Incorrect username or password.'
+    );
+  });
 });
