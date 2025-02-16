0e7cb1a2da710311845ca510a4f289eed6038f6d
Kiryl <kirilaf78@gmail.com>
2025-02-16 02:00:56 +0100

    some spec files added

---
+++ tests/Chapter01/03.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('My manual PlayWright', async ({ page }) => {
+  await test.step('Go to url', async () => {
+    await page.goto('https://www.youtube.com/');
+    await page
+      .getByRole('button', { name: 'Accept the use of cookies and' })
+      .click();
+  });
+
+  await test.step('Search with keywords', async () => {
+    await page
+      .getByRole('combobox', { name: 'Search' })
+      .fill('Finland vs Sweden');
+    await page.getByRole('combobox', { name: 'Search' }).press('Enter');
+  });
+
+  await expect(page).toHaveTitle('Finland vs Sweden - YouTube');
+});
