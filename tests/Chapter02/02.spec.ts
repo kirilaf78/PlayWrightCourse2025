665577ffd260f51dda5a15ad514bf4bec4fad273
Kiryl <kirilaf78@gmail.com>
2025-03-01 23:26:24 +0100

    how to user locators

--- tests/Chapter02/02.spec.ts
+++ tests/Chapter02/02.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Locators in PlayWright', async ({ page }) => {
-  await page.goto('https://github.com/BakkappaN');
+  // await page.goto('https://github.com/BakkappaN');
   //GetByRole
   // await page.getByRole('link', { name: 'Sign in' }).click();
 
@@ -12,5 +12,19 @@ test('Locators in PlayWright', async ({ page }) => {
   //await page.getByAltText("View BakkappaN's full-sized avatar").click();
   //GetByTestId
 
-  await page.getByTestId('repositories').first().click();
+  //await page.getByTestId('repositories').first().click();
+  //Get by text
+  // await page.getByText('Sign up').click();
+
+  //Get by placeholder, Xpath and Css
+  //await page.goto('https://youtube.com/@testerstalk');
+  //await page.getByRole('button', { name: 'Accept all' }).click();
+  //await page.getByPlaceholder('Search').fill('testers talk');
+  //await page.locator('//input[@name="search_query"]').fill('tester talk')
+  //await page.locator('input[name="search_query"]').fill('tester talk')
+
+  //Get by title
+  await page.goto('https://google.com/');
+  await page.getByRole('button', { name: 'Az összes elfogadása' }).click();
+  await page.getByTitle('Keresés').fill('some');
 });
