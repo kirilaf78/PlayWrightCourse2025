2149fe7fcfe5e0bbd1a6f80dd3519a0450c3c3f4
Kiryl <kirilaf78@gmail.com>
2025-03-06 21:50:49 +0100

    date picker

--- tests/Chapter02/08.spec.ts
+++ tests/Chapter02/08.spec.ts
@@ -2,13 +2,31 @@ import { test, expect } from '@playwright/test';
 
 test('Date picker', async ({ page }) => {
   await test.step('Go to url', async () => {
-    await page.goto('https://www.youtube.com/');
-
+    await page.goto('https://jqueryui.com/datepicker/');
   });
 
-  await test.step('Press Enter', async () => {
+  //   await test.step('Enter manually', async () => {
+  //     const iframe = page.frameLocator('[class="demo-frame"]');
+  //     await iframe.locator('[id="datepicker"]').fill('03/05/2025');
+  //   });
 
+  //   await test.step('Enter dynamically', async () => {
+  //     const iframe = page.frameLocator('[class="demo-frame"]');
+  //     await iframe.locator('[id="datepicker"]').click();
+  //     await iframe.locator('.ui-datepicker-today').click()
+  //   });
+
+//   await test.step('Previous date', async () => {
+//     const iframe = page.frameLocator('[class="demo-frame"]');
+//     await iframe.locator('[id="datepicker"]').click();
+//     await iframe.locator('[title="Prev"]').click();
+//     await iframe.locator('text="15"').click();
+//   });
+
+    await test.step('Next date', async () => {
+    const iframe = page.frameLocator('[class="demo-frame"]');
+    await iframe.locator('[id="datepicker"]').click();
+    await iframe.locator('[title="Next"]').click();
+    await iframe.locator('text="15"').click();
   });
-
-
- });
+});
