b8b00a50d67faa7987b5a084d1f30d42fdc89899
Kiryl <kirilaf78@gmail.com>
2025-03-26 21:40:41 +0100

    handling alerts

--- tests/Chapter04/03.spec.ts
+++ tests/Chapter04/03.spec.ts
@@ -1,21 +1,44 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("Await pop-ups", async ({ page }) => {
-  await test.step("Go to url", async () => {
+test('Await pop-ups', async ({ page }) => {
+  await test.step('Click the alert button', async () => {
     await page.goto(
-      "https://www.selenium.dev/documentation/webdriver/interactions/alerts/"
+      'https://www.selenium.dev/documentation/webdriver/interactions/alerts/'
     );
-  });
 
-  await test.step("Click the dialog button", async () => {
-    page.once('dialog', dialog =>{
+    page.once('dialog', (dialog) => {
       dialog.accept();
-      console.log(`The alert message is ${dialog.message()}`);
-      
-    })
+      console.log(`Dialog type is ${dialog.type()}`);
 
-    await page.getByText('See an example alert', { exact: true }).click()
+      console.log(`The alert message is ${dialog.message()}`);
+    });
+    await page.getByText('See an example alert', { exact: true }).click();
   });
 
-  //await expect(page).toHaveTitle('Finland vs Sweden - YouTube');
+  await test.step('Click the confirm button', async () => {
+    await page.goto(
+      'https://www.selenium.dev/documentation/webdriver/interactions/alerts/'
+    );
+
+    page.once('dialog', (dialog) => {
+      dialog.accept();
+      console.log(`Dialog type is ${dialog.type()}`);
+
+      console.log(`The confirm message is ${dialog.message()}`);
+    });
+    await page.getByText('See a sample confirm', { exact: true }).click();
+  });
+
+  await test.step('Click the prompt button', async () => {
+    await page.goto(
+      'https://www.selenium.dev/documentation/webdriver/interactions/alerts/'
+    );
+
+    page.once('dialog', async (dialog) => {
+      await dialog.accept('playwright');
+      console.log(`The prompt message is ${dialog.message()}`);
+      console.log(`Dialog type is ${dialog.type()}`);
+    });
+    await page.getByText('See a sample prompt', { exact: true }).click();
+  });
 });
